1f38038bed35dd37d84ad9896f0d445399fa657e
Kamil Ogórek <kamil.ogorek@gmail.com>
2025-05-01 15:33:08 +0200

    vim -> nvim; zed updates

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ brew update
 brew upgrade
 
 echo "=> Installing Homebrew packages"
-for package in ag atuin bat cask coreutils curl diff-so-fancy eza gh git fd ffmpeg fzf httpie jq kondo n ncdu neovim ngrok/ngrok/ngrok vim yt-dlp/taps/yt-dlp z zsh
+for package in ag atuin bat cask coreutils curl diff-so-fancy eza gh git fd ffmpeg fzf httpie jq kondo n ncdu neovim ngrok/ngrok/ngrok nvim yt-dlp/taps/yt-dlp z zsh
 do
     brew install $package
 done
